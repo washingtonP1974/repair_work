6a8eb5d499b8352fca668f86cf4dced51abdebfd
morgan/bin/bash <55928887+washingtonP1974@users.noreply.github.com>
2023-05-27 17:45:37 -0300

    Update repairwork.sh

--- repairwork.sh
+++ repairwork.sh
@@ -3,16 +3,16 @@ free -h
 sudo swapoff -av ; sudo swapon -av
 sudo du -h /var/cache/apt/archives
 sudo sync; echo 1 > /proc/sys/vm/drop_caches
-sysctl -w vm.drop_caches=1
+sudo sysctl -w vm.drop_caches=1
 sudo sync; echo 2 > /proc/sys/vm/drop_caches
-sysctl -w vm.drop_caches=2
-sync && sudo sysctl -w vm.drop_caches=1
-sync && sudo sysctl -w vm.drop_caches=2
+sudo sysctl -w vm.drop_caches=2
+sudo sync && sudo sysctl -w vm.drop_caches=1
+sudo sync && sudo sysctl -w vm.drop_caches=2
 sudo apt-get clean && sudo apt-get autoclean
 sudo journalctl --disk-usage
 sudo journalctl --vacuum-time=30d
-du -sh ~/.cache/thumbnails
-rm -rf ~/.cache/thumbnails/*
+sudo du -sh ~/.cache/thumbnails
+sudo rm -rf ~/.cache/thumbnails/*
 sudo rm -rf /home/$USER/.local/share/Trash/files/*
 sudo rm -rf /var/tmp/*
 sudo apt-get clean
@@ -23,7 +23,6 @@ sudo dpkg --configure -a
 sudo apt-get -f install
 sudo du -h /var/cache/apt/archives
 sudo du -sh /var/cache/apt
-sudo dpkg --list 'linux-image*'
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get full-upgrade
